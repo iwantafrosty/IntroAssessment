55e94de4d5c120abd805d345fa2e6bdab61bbc7b
iwantafrosty <131530255+iwantafrosty@users.noreply.github.com>
2024-01-28 10:07:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let firstName;
+let lastName;
+let age;
+
+firstName = 'Michael';
+lastName = 'Reyes';
+age = 22;
+
+alert('My name is ' + firstName + ' ' + lastName);
+alert('I am ' + age + ' years old');
+
+console.log('I am having a lot of fun in this class!');
